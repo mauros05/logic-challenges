242e8ba38451db0e928d3dcf37c4bf9669fc8425
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-08-01 23:16:59 -0600

    Update variable names

--- clock.rb
+++ clock.rb
@@ -1,8 +1,9 @@
 def time_to_degrees(hour)
   # First step: Divide the time in hours and minutes
-  time_elements = hour.split(":")
-  hour          = time_elements[0].to_i
-  minutes       = time_elements[1].to_i
+  time    = hour.split(":")
+  hour    = time[0].to_i
+  minutes = time[1].to_i
 
   # Second step: Pass the hour to degrees
+  hour_to_degrees = hour * 30
 end
